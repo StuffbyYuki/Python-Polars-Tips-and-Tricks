537f2731484d177b04301f63d59644c632f315cf
stuffbyyuki <kake.19940627@gmail.com>
2023-02-20 22:37:38 -0700

    code completed

--- pandas_vs_polars/main.py
+++ pandas_vs_polars/main.py
@@ -1,11 +1,14 @@
 import polars as pl
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 import time
 import sys 
 sys.path.insert(1, '../')
-from config import file_path_10million_rows_csv
+from config import file_path_10million_rows_csv, file_path_1million_rows_csv
 
 FILE_PATH = file_path_10million_rows_csv
+FILE_PATH_2 = file_path_1million_rows_csv
 
 def read_polars(file_path):
     '''
@@ -100,23 +103,19 @@ def window_func_pandas(pd_df):
     
     return output
 
-def inner_join_polars():
-    return
+def join_polars(pl_df1, pl_df2, on_column, join_type):
+    '''
+    a func to join in polars based on inputs
+    '''
+    output = pl_df1.lazy().join(pl_df2.lazy(), on=on_column, how=join_type).collect()
+    return output
 
-def left_outer_join_polars():
-    return
-
-def full_outer_join_polars():
-    return
-
-def inner_join_pandas():
-    return
-
-def left_outer_join_pandas():
-    return
-
-def full_outer_join_pandas():
-    return
+def join_pandas(pd_df1, pd_df2, on_column, join_type):
+    '''
+    a func to join in pandas based on inputs
+    '''
+    output = pd_df1.merge(pd_df2, on=on_column, how=join_type)
+    return output
 
 def time_a_function(func, *args):
     '''
@@ -128,40 +127,85 @@ def time_a_function(func, *args):
     result = round(end_time - start_time, 2)
     return result
 
+def test_pandas_vs_polars(pl_func, pd_func, pl_func_params, pd_func_params):
+    '''
+    testing performance on pandas func vs polars func
+    '''
+    pl_time = time_a_function(pl_func, *pl_func_params)
+    print(f'Polars took {pl_time} seconds.')
+    pd_time = time_a_function(pd_func, *pd_func_params)
+    print(f'Pandas took {pd_time} seconds.')
+    
+    return pl_time, pd_time
+
 def main():
 
     ##### read a csv file #####
-    # print('Reading 10 million rows with 14 columns (1.2GB)...')
-
-    # pl_time = time_a_function(read_polars, FILE_PATH)
-    # print(f'Polars took {pl_time} seconds.')
-
-    # pd_time = time_a_function(read_pandas, FILE_PATH)
-    # print(f'Pandas took {pd_time} seconds.')
-
+    print('Reading 10 million rows with 14 columns (1.2GB)...')
+    pl_read_csv_time, pd_read_csv_time = test_pandas_vs_polars(read_polars, read_pandas, [FILE_PATH], [FILE_PATH])
 
     ##### simple aggregations #####
-    # print('Testing performance on aggregations...')
-    # pl_df = read_polars(FILE_PATH)
-    # pl_time = time_a_function(agg_polars, pl_df)
-    # print(f'Polars took {pl_time} seconds.')
-    
-    # pd_df = read_pandas(FILE_PATH)
-    # pd_time = time_a_function(agg_pandas, pd_df)
-    # print(f'Pandas took {pd_time} seconds.')
+    #  preapre dfs
+    pl_df1 = read_polars(FILE_PATH)
+    pl_df2 = read_polars(FILE_PATH_2)
+    pd_df1 = read_pandas(FILE_PATH)
+    pd_df2 = read_pandas(FILE_PATH_2)
 
+    print('\nTesting performance on aggregations...')
+    pl_agg_time, pd_agg_time = test_pandas_vs_polars(agg_polars, agg_pandas, [pl_df1], [pd_df1])
 
     ##### window functions #####
-    print('Testing performance on window functions...')
-    pl_df = read_polars(FILE_PATH)
-    pl_time = time_a_function(window_func_polars, pl_df)
-    print(f'Polars took {pl_time} seconds.')
+    print('\nTesting performance on window functions...')
+    pl_window_time, pd_window_time = test_pandas_vs_polars(window_func_polars, window_func_pandas, [pl_df1], [pd_df1])
     
-    pd_df = read_pandas(FILE_PATH)
-    pd_time = time_a_function(window_func_pandas, pd_df)
-    print(f'Pandas took {pd_time} seconds.')
+    ##### join operations #####
+    print('\nTesting performance on joins...')
+
+    print('--- Inner join --- ')
+    pl_inner_join_time, pd_inner_join_time = test_pandas_vs_polars(join_polars, join_pandas, [pl_df1, pl_df2, 'email', 'inner'], [pd_df1, pd_df2, 'email', 'inner'])
+
+    print('--- Left outer join ---')
+    pl_left_join_time, pd_left_join_time = test_pandas_vs_polars(join_polars, join_pandas, [pl_df1, pl_df2, 'email', 'left'], [pd_df1, pd_df2, 'email', 'left'])
+
+    print('--- Full outer join ---')
+    pl_full_join_time, pd_full_join_time = test_pandas_vs_polars(join_polars, join_pandas, [pl_df1, pl_df2, 'email', 'outer'], [pd_df1, pd_df2, 'email', 'outer'])
+
+    ##### visualize result #####
+    # sns.set(rc={'figure.dpi':300, 'savefig.dpi':300})
+    sns.set_style(style=None)
+
+    data = [
+        # polars
+        [pl_read_csv_time, 'read csv', 'polars'], 
+        [pl_agg_time, 'aggregations', 'polars'],
+        [pl_window_time, 'window functions', 'polars'],
+        [pl_inner_join_time, 'inner join', 'polars'],
+        [pl_left_join_time, 'left join', 'polars'],
+        [pl_full_join_time, 'full join', 'polars'],
+        # pandas
+        [pd_read_csv_time, 'read csv', 'pandas'], 
+        [pd_agg_time, 'aggregations', 'pandas'],
+        [pd_window_time, 'window functions', 'pandas'],
+        [pd_inner_join_time, 'inner join', 'pandas'],
+        [pd_left_join_time, 'left join', 'pandas'],
+        [pd_full_join_time, 'full join', 'pandas'],
+    ]
+
+    df = pl.DataFrame(data, schema=['time in seconds', 'test type', 'library'])
+
+    plt.figure(figsize=(20, 6))
+    ax = sns.barplot(x=list(df['test type']), y=list(df['time in seconds']), hue=list(df['library']), errorbar=None, palette=['#3063A5', '#d3d3d3'])
+    # adding labels
+    for container in ax.containers:
+        ax.bar_label(container)
+    ax.set(xlabel='', ylabel='Time in Seconds')
+    plt.title('Polars vs Pandas - Speed Comparison')
+    plt.show()
+
+    # export to png
+    fig = ax.get_figure()
+    fig.savefig('polars vs pandas - python script.png') 
     
-    # join operations
 
 if __name__ == '__main__':
     main()
