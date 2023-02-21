bcaef13c564505cebcc5592ccce62ee1e5b55374
stuffbyyuki <kake.19940627@gmail.com>
2023-02-21 16:05:37 -0700

    code modfiied

--- convert_df_to_s/convert_df_to_s.py
+++ convert_df_to_s/convert_df_to_s.py
@@ -9,9 +9,13 @@ print(type(df))
 series_1 = df.select('ID').to_series()
 print(type(series_1))
 
-# 2. just taking a column as a series
-series_2 = df['ID']
+# 2. using .to_series() alone, column index specified
+series_2 = df.to_series(2)
 print(type(series_2))
+
+# 3. just taking a column as a series
+series_3 = df['ID']
+print(type(series_3))
 
 # .select() itself returns a dataframe
 this_is_df = df.select('ID')
