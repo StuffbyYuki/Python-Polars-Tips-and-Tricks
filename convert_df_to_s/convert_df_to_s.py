c293ec3456a7b266a4911dcaa8a32ab4ffb36ec3
stuffbyyuki <kake.19940627@gmail.com>
2023-02-21 15:46:00 -0700

    code completed

---
+++ convert_df_to_s/convert_df_to_s.py
@@ -0,0 +1,18 @@
+import polars as pl
+
+# creating dummy data
+data = [[1, 2, 3], ['A', 'B', 'C'], [111, 222, 333]]
+df = pl.DataFrame(data, schema=['ID', 'Letter', 'Values'])
+print(type(df))
+
+# 1. using to.series()
+series_1 = df.to_series()
+print(type(series_1))
+
+# 2. just taking a column as a series
+series_2 = df['ID']
+print(type(series_2))
+
+# .select() returns a dataframe
+this_is_df = df.select('ID')
+print(type(this_is_df))
