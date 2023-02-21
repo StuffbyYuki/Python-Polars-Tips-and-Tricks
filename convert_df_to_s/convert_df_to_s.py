e84ab44304664f7b19da4e44270438d5f04486e2
stuffbyyuki <kake.19940627@gmail.com>
2023-02-21 15:59:15 -0700

    modified code

--- convert_df_to_s/convert_df_to_s.py
+++ convert_df_to_s/convert_df_to_s.py
@@ -5,14 +5,14 @@ data = [[1, 2, 3], ['A', 'B', 'C'], [111, 222, 333]]
 df = pl.DataFrame(data, schema=['ID', 'Letter', 'Values'])
 print(type(df))
 
-# 1. using to.series()
-series_1 = df.to_series()
+# 1. using .to_series() on .select()
+series_1 = df.select('ID').to_series()
 print(type(series_1))
 
 # 2. just taking a column as a series
 series_2 = df['ID']
 print(type(series_2))
 
-# .select() returns a dataframe
+# .select() itself returns a dataframe
 this_is_df = df.select('ID')
 print(type(this_is_df))
