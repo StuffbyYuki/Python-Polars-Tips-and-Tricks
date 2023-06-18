436b4bcead1b0d5495e6ac96c1dcdc8f132e7186
stuffbyyuki <kake.19940627@gmail.com>
2023-06-17 23:27:56 -0600

    added second reading option

--- s3/s3_polars.py
+++ s3/s3_polars.py
@@ -11,10 +11,18 @@ dataset = ds.dataset(f"s3://{BUCKET_NAME}/order_extract.parquet", filesystem=fs,
 df_parquet = pl.scan_pyarrow_dataset(dataset)
 print(df_parquet.collect().head())
 
+# read parquet 2 
+with fs.open(f'{BUCKET_NAME}/order_extract.parquet', mode='rb') as f:
+    print(pl.read_parquet(f).head())
+          
 # read csv
 dataset = ds.dataset(f"s3://{BUCKET_NAME}/order_extract.csv", filesystem=fs, format='csv')
 df_csv = pl.scan_pyarrow_dataset(dataset)
 print(df_csv.collect().head())
+
+# read csv 2 
+with fs.open(f'{BUCKET_NAME}/order_extract.csv', mode='rb') as f:
+    print(pl.read_csv(f).head())
 
 # prep df
 df = pl.DataFrame({
@@ -29,4 +37,3 @@ with fs.open(f'{BUCKET_NAME}/direction.parquet', mode='wb') as f:
 # write csv
 with fs.open(f'{BUCKET_NAME}/direction.csv', mode='wb') as f:
     df.write_csv(f)
-    
